dbb65153c67a51863e4e207e225509bf37577a34
msrdnv <msrdnv@ya.ru>
2023-03-19 17:53:42 +0300

    fix: auto popup close after script

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-let editButton = document.querySelector('.profile__edit-button');
-let closeIcon = document.querySelector('.popup__close-icon');
 let popup = document.querySelector('.popup');
-
-let popupForm = document.querySelector('.popup__container');
 
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
+
+let editButton = document.querySelector('.profile__edit-button');
+let closeIcon = document.querySelector('.popup__close-icon');
+
+let popupForm = document.querySelector('.popup__container');
+
 let popupNameInput = document.querySelector('.popup__name');
 let popupAboutInput = document.querySelector('.popup__about');
-
 
 editButton.addEventListener('click', function () {
   popup.classList.add('popup_opened');
@@ -24,6 +25,7 @@ function handleFormSubmit (event) {
   event.preventDefault();
   profileName.textContent = popupNameInput.value;
   profileAbout.textContent = popupAboutInput.value;
+  popup.classList.remove('popup_opened');
 }
 
 popupForm.addEventListener('submit', handleFormSubmit);
