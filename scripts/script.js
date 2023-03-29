353357722bee0aeec2f95a75f71bd52312e0b9fb
msrdnv <msrdnv@ya.ru>
2023-03-29 20:30:41 +0300

    refactor: change names

--- scripts/script.js
+++ scripts/script.js
@@ -39,9 +39,9 @@ for (let i = initialCards.length; i > 0; i--) {
   addCardPrepend(initialCards[i-1]);
 }
 
-function openEditFormPopup() {
+function openEditNamePopup() {
   popup.classList.add('popup_opened');
-  popupEditFormContainer.classList.add('popup__container_opened');
+  popupEditNameContainer.classList.add('popup__container_opened');
 }
 
 function openAddCardPopup() {
@@ -51,36 +51,36 @@ function openAddCardPopup() {
 
 function closePopup() {
   popup.classList.remove('popup_opened');
-  popupEditFormContainer.classList.remove('popup__container_opened');
+  popupEditNameContainer.classList.remove('popup__container_opened');
   popupAddCardContainer.classList.remove('popup__container_opened');
 }
 
 const popup = document.querySelector('.popup');
 const popupTemplate = document.querySelector('#popup-container').content;
 
-const popupEditFormContainer = popupTemplate.querySelector('.popup__container').cloneNode(true);
-const popupEditFormCloseIcon = popupEditFormContainer.querySelector('.popup__close-icon');
-popupEditFormContainer.querySelector('.popup__title').textContent = 'Редактировать профиль';
-const popupEditForm = popupEditFormContainer.querySelector('.popup__form');
-popupEditForm.name = 'popup-edit-form';
-const popupEditInput = popupEditFormContainer.querySelectorAll('.popup__input');
-popupEditInput[0].name = 'input-name';
-popupEditInput[1].name = 'input-about';
-popupEditInput[0].placeholder = 'Имя';
-popupEditInput[1].placeholder = 'О себе';
-popupEditFormContainer.querySelector('.popup__submit-button').textContent = 'Сохранить';
-popup.append(popupEditFormContainer);
+const popupEditNameContainer = popupTemplate.querySelector('.popup__container').cloneNode(true);
+const popupEditNameCloseIcon = popupEditNameContainer.querySelector('.popup__close-icon');
+popupEditNameContainer.querySelector('.popup__title').textContent = 'Редактировать профиль';
+const popupEditNameForm = popupEditNameContainer.querySelector('.popup__form');
+popupEditNameForm.name = 'popup-edit-name-form';
+const popupEditNameFormInput = popupEditNameContainer.querySelectorAll('.popup__input');
+popupEditNameFormInput[0].name = 'input-name';
+popupEditNameFormInput[1].name = 'input-about';
+popupEditNameFormInput[0].placeholder = 'Имя';
+popupEditNameFormInput[1].placeholder = 'О себе';
+popupEditNameContainer.querySelector('.popup__submit-button').textContent = 'Сохранить';
+popup.append(popupEditNameContainer);
 
 const popupAddCardContainer = popupTemplate.querySelector('.popup__container').cloneNode(true);
 const popupAddCardCloseIcon = popupAddCardContainer.querySelector('.popup__close-icon');
 popupAddCardContainer.querySelector('.popup__title').textContent = 'Новое место';
 const popupAddCardForm = popupAddCardContainer.querySelector('.popup__form');
 popupAddCardForm.name = 'popup-add-card-form';
-const popupAddInput = popupAddCardContainer.querySelectorAll('.popup__input');
-popupAddInput[0].name = 'input-card-name';
-popupAddInput[1].name = 'input-card-link';
-popupAddInput[0].placeholder = 'Название';
-popupAddInput[1].placeholder = 'Ссылка на картинку';
+const popupAddCardFormInput = popupAddCardContainer.querySelectorAll('.popup__input');
+popupAddCardFormInput[0].name = 'input-card-name';
+popupAddCardFormInput[1].name = 'input-card-link';
+popupAddCardFormInput[0].placeholder = 'Название';
+popupAddCardFormInput[1].placeholder = 'Ссылка на картинку';
 popupAddCardContainer.querySelector('.popup__submit-button').textContent = 'Создать';
 popup.append(popupAddCardContainer);
 
@@ -90,9 +90,9 @@ const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__about');
 
 editButton.addEventListener('click', function () {
-  popupEditInput[0].value = profileName.textContent;
-  popupEditInput[1].value = profileAbout.textContent;
-  openEditFormPopup();
+  popupEditNameFormInput[0].value = profileName.textContent;
+  popupEditNameFormInput[1].value = profileAbout.textContent;
+  openEditNamePopup();
 });
 
 addButton.addEventListener('click', function() {
@@ -103,26 +103,26 @@ popupAddCardCloseIcon.addEventListener('click', function() {
   closePopup();
 })
 
-popupEditFormCloseIcon.addEventListener('click', function() {
+popupEditNameCloseIcon.addEventListener('click', function() {
   closePopup();
 })
 
-function submitEditForm (event) {
+function submitEditNameForm (event) {
   event.preventDefault();
-  profileName.textContent = popupEditInput[0].value;
-  profileAbout.textContent = popupEditInput[1].value;
+  profileName.textContent = popupEditNameFormInput[0].value;
+  profileAbout.textContent = popupEditNameFormInput[1].value;
   closePopup();
 };
 
-function submitAddCard (event) {
+function submitAddCardForm (event) {
   event.preventDefault();
   const newCard = {
-    name: popupAddInput[0].value,
-    link: popupAddInput[1].value,
+    name: popupAddCardFormInput[0].value,
+    link: popupAddCardFormInput[1].value,
   };
   addCardPrepend(newCard);
   closePopup();
 }
 
-popupEditForm.addEventListener('submit', submitEditForm);
-popupAddCardForm.addEventListener('submit', submitAddCard);
+popupEditNameForm.addEventListener('submit', submitEditNameForm);
+popupAddCardForm.addEventListener('submit', submitAddCardForm);
